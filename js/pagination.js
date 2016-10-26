58a2ee1ff63590dc2cba0628c8f208fee2fa6f0c
Ryan Osten <ostenrj@gmail.com>
2016-10-26 08:36:22 -0400

    added keyup event to search and improved search results

--- js/pagination.js
+++ js/pagination.js
@@ -1,23 +1,22 @@
-//store a jQuery object representing the student list
-var studentListItems = $('.student-list li');
-
 //number of items that will appear on a page
 var itemsPerPage = 10;
-
-//count number of students, and divide by 10 to get number of pages required to build pagination
-var numPages = Math.ceil(studentListItems.length / itemsPerPage);
 
 //we will use this to build a string to add the page links to
 var pages = '';
 
-//hide all elements after 10th element
-studentListItems.slice(itemsPerPage).hide();
+//hide all elements after itemPerPage
+$(document).ready(function(){
+//store a jQuery element representing the student list
+var studentListItems = $('.student-list li');
+studentListItems.slice(itemsPerPage).hide();	
 
-//add pagination structure
+})
+
+//add pagination elements
 $('.student-list').after('<div class="pagination"><ul></ul></div>');
 
 //build string for pagination buttons
-for(i = 1; i <= numPages; i++){
+for(i = 1; i <= Math.ceil($('.student-list li').length / itemsPerPage); i++){
 	pages += '<li>'
 	pages += '<a href="#">' + i + '</a>'
 	pages += '</li>'
@@ -26,15 +25,14 @@ for(i = 1; i <= numPages; i++){
 //add the pages list items to the unordered pagination list
 $('.pagination').children().html(pages);
 
-//click handler that checks the page link clicked and displays the appropriate list of students
+//This click handler will store the text of the html element clicked and convert it to an integer
+//Then hide list of students, and then use slice to show list of students based on page clicked
 $('.pagination ul li').on("click", function(){
-	//get the text of the html element clicked and convert it to an integer
+	//count number of students, and divide by 10 to get number of pages required to build pagination
+	var numPages = Math.ceil(studentListItems.length / itemsPerPage);
 	var pageClicked = parseInt($(this).text());
-	
-	//hide list of students
-	studentListItems.hide();
-	//show list of students based on page link clicked
-	studentListItems.slice(itemsPerPage*pageClicked-itemsPerPage, itemsPerPage*pageClicked).show();
+	$(studentListItems).hide();
+	$(studentListItems).slice(itemsPerPage*pageClicked-itemsPerPage, itemsPerPage*pageClicked).show();
 });
 
 
--- js/search.js
+++ js/search.js
@@ -1,29 +1,52 @@
 //dynamically add the search box to the DOM
 var btn = $(".page-header").children().first().after('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
-//store student list items
-var studentListItems = $('.student-list li')
 
+var studentListItems = [];
 //make click handler on search button. Hide all students, then store the search value.
 //then loop through all student list and check if search value matches any students. If there is a match, then show student. 
-$('.student-search').find('button').on("click", function(){
-	
-	studentListItems.hide()
-	
+
+$('.student-search').find('input').on("keyup", function(){
+
+	$('.student-list li').each(function(i){
+		$(this).hide()
+	});
+
+	studentListItems = [];
+
 	//store value in search input box
 	var searchValue = $(".student-search").find("input").val().toLowerCase();
 
 	//loop through all student list items, store the name and email for each student, then compare name and email to search value
 	//if searchValue matches name or email, show student list item
-	$('.student-list').children().each(function(i){
+	$('.student-list').children().each(function(){
 		var studentName = $(this).find("h3").text();
 		var studentEmail = $(this).find(".email").text();
 
 		if(studentName.indexOf(searchValue) != -1 || studentEmail.indexOf(searchValue) != -1 ){
-			$(this).show()
+			studentListItems.push(this);
 		}
 	});
-	
+
+	$(studentListItems).each(function(){
+		$(this).show();
+	})
+		$(studentListItems).slice(itemsPerPage).hide();
+		$('.pagination ul').children().slice(Math.ceil(studentListItems.length / itemsPerPage)).hide();	
+		
 })
+
+
+
+
+/*
+		$(searchResults).each(function(i){
+			$(this).show()
+			var numPages = Math.ceil(searchResults.length / itemsPerPage);
+			$('.pagination ul').children().slice(numPages).hide();			
+		})
+		*/
+
+		//store # of search results and do magic to hide/show pagination accordingly
 
 
 
