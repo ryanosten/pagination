4ab51b55584e129c91edd38a9e72687161f94d87
Ryan Osten <ostenrj@gmail.com>
2016-10-27 18:25:59 -0400

    cleaned up a few errors

--- js/pagination.js
+++ js/pagination.js
@@ -1,8 +1,17 @@
+/* GLOBAL VARIABLES */
+
 //number of items that will appear on a page
 var itemsPerPage = 10;
 
 //we will use this to build a string to add the page links to
 var pages = '';
+
+//dynamically add the search input to the DOM
+$(".page-header").children().first().after('<div class="student-search"><input placeholder="Search for students..."></div>');
+
+var studentListItems = [];
+
+/* EVENT HANDLERS */
 
 //hide all elements after itemPerPage
 $(document).ready(function(){
@@ -10,30 +19,31 @@ $(document).ready(function(){
 var studentListItems = $('.student-list li');
 studentListItems.slice(itemsPerPage).hide();	
 
-})
+});
+
+//This click handler will store the text of the html element clicked and convert it to an integer
+//Then hide list of students, and then use slice to show list of students based on page clicked
+$('.pagination ul li').on("click", function(){
+	//count number of students, and divide by itemsPerPage to get number of pages required to build pagination
+	var pageClicked = parseInt($(this).text());
+	$(studentListItems).hide();
+	$(studentListItems).slice(itemsPerPage*pageClicked-itemsPerPage, itemsPerPage*pageClicked).show();
+});
+
 
 //add pagination elements
 $('.student-list').after('<div class="pagination"><ul></ul></div>');
 
 //build string for pagination buttons
 for(i = 1; i <= Math.ceil($('.student-list li').length / itemsPerPage); i++){
-	pages += '<li>'
-	pages += '<a href="#">' + i + '</a>'
-	pages += '</li>'
+	pages += '<li>';
+	pages += '<a href="#">' + i + '</a>';
+	pages += '</li>';
 } 
 
 //add the pages list items to the unordered pagination list
 $('.pagination').children().html(pages);
 
-//This click handler will store the text of the html element clicked and convert it to an integer
-//Then hide list of students, and then use slice to show list of students based on page clicked
-$('.pagination ul li').on("click", function(){
-	//count number of students, and divide by 10 to get number of pages required to build pagination
-	var numPages = Math.ceil(studentListItems.length / itemsPerPage);
-	var pageClicked = parseInt($(this).text());
-	$(studentListItems).hide();
-	$(studentListItems).slice(itemsPerPage*pageClicked-itemsPerPage, itemsPerPage*pageClicked).show();
-});
 
 
 
--- js/search.js
+++ js/search.js
@@ -1,15 +1,11 @@
-//dynamically add the search box to the DOM
-$(".page-header").children().first().after('<div class="student-search"><input placeholder="Search for students..."></div>');
+/* EVENT HANDLERS */
 
-
-var studentListItems = [];
 //make click handler on search button. Hide all students, then store the search value.
 //then loop through all student list and check if search value matches any students. If there is a match, then show student. 
-
 $('.student-search').find('input').on("keyup", function(){
 
 	$('.student-list li').each(function(i){
-		$(this).hide()
+		$(this).hide();
 	});
 
 	studentListItems = [];
@@ -18,7 +14,7 @@ $('.student-search').find('input').on("keyup", function(){
 	var searchValue = $(".student-search").find("input").val().toLowerCase();
 
 	//loop through all student list items, store the name and email for each student, then compare name and email to search value
-	//if searchValue matches name or email, show student list item
+	//if searchValue matches name or email, push the object to the studentListItems array
 	$('.student-list').children().each(function(){
 		var studentName = $(this).find("h3").text();
 		var studentEmail = $(this).find(".email").text();
@@ -28,28 +24,25 @@ $('.student-search').find('input').on("keyup", function(){
 		}
 	});
 
+	//show list of students
 	$(studentListItems).each(function(){
 		$(this).show();
-	})
+	});
 	
+	//hide all elements in array indexed after the itemPerPage value
 	$(studentListItems).slice(itemsPerPage).hide();
+
+	//show all pagination list items (page links)
 	$('.pagination ul').children().show();
+	//hide pagination list items (page links)that shouldn't be displayed
 	$('.pagination ul').children().slice(Math.ceil(studentListItems.length / itemsPerPage)).hide();
 		
-})
+});
 
 
 
 
-/*
-		$(searchResults).each(function(i){
-			$(this).show()
-			var numPages = Math.ceil(searchResults.length / itemsPerPage);
-			$('.pagination ul').children().slice(numPages).hide();			
-		})
-		*/
 
-		//store # of search results and do magic to hide/show pagination accordingly
 
 
 
